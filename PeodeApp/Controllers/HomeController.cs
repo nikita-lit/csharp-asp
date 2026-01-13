fd333c7a4b233f5aeb8f38c67ec87330b4f9a3b1
Nikita Litvinenko <nikitalitvinenko28@gmail.com>
2026-01-13 15:15:23 +0200

    Update

--- PeodeApp/Controllers/HomeController.cs
+++ PeodeApp/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using PeodeApp.Data;
 using PeodeApp.Models;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
 
 namespace PeodeApp.Controllers
 {
@@ -34,6 +36,17 @@ namespace PeodeApp.Controllers
 
         public IActionResult Tanan(Kylaline kylaline)
         {
+            if (kylaline == null)
+                return NotFound();
+
+            ViewBag.PyhaNimi = _context.Pyhad.Find(kylaline.PyhaID)?.Nimi;
+            if (kylaline.OnKutse)
+                ViewBag.Pilt = "kolmas.jpg";
+            else
+                ViewBag.Pilt = "close_box_red.png";
+
+            SaadaEmail(kylaline, ViewBag.Pilt, ViewBag.PyhaNimi);
+
             return View(kylaline);
         }
 
@@ -46,16 +59,72 @@ namespace PeodeApp.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Ankeet(Kylaline kylaline)
         {
             if(ModelState.IsValid)
             {
                 _context.Kylalined.Add(kylaline);
                 _context.SaveChanges();
+
                 return RedirectToAction("Tanan", kylaline);
             }
 
             return View(kylaline);
+        }
+        
+        private void SaadaEmail(Kylaline kylaline, string pilt, string pyha)
+        {
+            string failiTee = Path.Combine(
+                Directory.GetCurrentDirectory(), // For ASP.NET Core, adjust path
+                "wwwroot",
+                "images",
+                pilt
+            );
+
+            try
+            {
+                string sisu;
+
+                if (kylaline.OnKutse)
+                {
+                    sisu = $"Tere, {kylaline.Nimi}!<br/><br/>" +
+                           $"Sinu registreerumine sündmusele <b>{pyha}</b> on salvestud.<br/>" +
+                           "Lisasime kirjale ka sündmuse kutse. Ootame sind väga!<br/><br/>" +
+                           "Kohtumiseni!";
+                }
+                else
+                {
+                    sisu = $"Tere, {kylaline.Nimi}!<br/><br/>" +
+                           $"Sinu registreerumine sündmusele <b>{pyha}</b> on salvestud.<br/>" +
+                           "Lisasime kirjale ka sündmuse kutse. Kahju, et sa ei saa tule peole!<br/><br/>" +
+                           "Kõige head!";
+                }
+
+                using (var mail = new MailMessage())
+                {
+                    mail.From = new MailAddress("nikitalitvinenko28@gmail.com", "Peofirma");
+                    mail.To.Add(kylaline.Email);
+                    mail.Subject = "Vastus: " + pyha;
+                    mail.Body = sisu;
+                    mail.IsBodyHtml = true;
+
+                    if (System.IO.File.Exists(failiTee))
+                        mail.Attachments.Add(new Attachment(failiTee));
+
+                    using (var smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.Credentials = new NetworkCredential("nikitalitvinenko28@gmail.com", "");
+                        smtp.EnableSsl = true;
+
+                        smtp.Send(mail);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
